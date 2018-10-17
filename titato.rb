9fa0e8b36df69f1fe4937ac6fa76496d796a8ed3
jango974 <40597435+jango974@users.noreply.github.com>
2018-10-17 12:18:39 +0200

    team correct

--- titato.rb
+++ titato.rb
@@ -1,148 +1,172 @@
+# frozen_string_literal: true                  #methode de la team
+
 require 'colorize'
-class Board
-  attr_accessor :board
-  def initialize
-    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  end
 
-  def look_board
-  puts
-  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-  puts "-----------"
-  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-  puts "-----------"
-  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  puts
-  end
-
-  def play(board)
-    if @board[board[2]] == " "
-      if board[1] == 'X'.red
-        @board[board[2]] << 'X'.red
-      else
-        @board[board[2]] << 'O'.blue
-      end
-   
-    end
-  end
-
-  def victory?
-    case
-    when @board[0] == @board[1] && @board[0] == @board[2] && @board[0] != " "
-      return true
-    when @board[3] == @board[4] && @board[3] == @board[5] && @board[3] != " "
-      return true
-    when @board[6] == @board[7] && @board[6] == @board[8] && @board[6] != " "
-      return true
-    when @board[0] == @board[3] && @board[0] == @board[6] && @board[0] != " "
-      return true
-    when @board[2] == @board[5] && @board[2] == @board[8] && @board[2] != " "
-      return true
-    when @board[0] == @board[4] && @board[0] == @board[8] && @board[0] != " "
-      return true
-    when @board[2] == @board[4] && @board[2] == @board[6] && @board[2] != " "
-      return true
-    when @board[1] == @board[4] && @board[1] == @board[7] && @board[1] != " "
-      return true
-    else
-      return false
-    end
-  end
-end
-
+# class Player
+# Est appele 2 fois pour enregistrer les joueurs
 class Player
-  attr_accessor :name, :player_choice
-  def initialize(name, player_choice)
-    @name = name
-    @player_choice = player_choice
+  attr_accessor :firstname
+
+  def initialize(firstname)
+    @firstname = firstname
+  end
+
+  def name
+    @firstname.to_s.capitalize
   end
 end
 
+# Class Board
+# Initialise le tableau a 0(vide)
+class Board
+  attr_accessor :tab
 
-class Game
   def initialize
-    p "joueur 1 quel est ton blase ?"
-    name_player1 = gets.chomp
-    p "#{name_player1} vous etes les 'X' "
-    player_choice1 = 'X'.red
-    @player1 = Player.new(name_player1, player_choice1)
-    p 'joueur 2 quel est ton blase ?'
-    name_player2 = gets.chomp
-    p "#{name_player2} vous etes les 'O'"
-    player_choice2 = 'O'.blue
-    @player2 = Player.new(name_player2, player_choice2)
-    @players = [@player1, @player2]
-    @actual_board = Board.new
-    turns = 0
+    @tab = []
+    @tab = [' ', ' ', ' ',     #  1 | 2 | 3
+            ' ', ' ', ' ',     #  4 | 5 | 6
+            ' ', ' ', ' ']     #  7 | 8 | 9
   end
 
-  def go
-    puts
-    puts 'Regles'
-    puts 'indique un numero'
-    puts
-    puts "  1 | 2 | 3 "
-    puts "-------------"
-    puts "  4 | 5 | 6 "
-    puts "-------------"
-    puts "  7 | 8 | 9 "
-    puts
-    self.turn
+  def base_board
+    @tab
   end
-  def turn
-    turns = 0
-    while   @actual_board.victory? == false do
-      @players.each{ |player|
-      @actual_board.look_board
-      p " #{turns + 1}e coup"
-      p "#{player.name} choisis une case "
-      @case = gets.chomp.to_i
-         
-    until @case.positive? && @case < 10
-      puts 'erreur'
-      puts 'Choisis une nouvelle case'
-      @case= gets.chomp.to_i
-    end
-      @case -= 1
-      @resume = [player.name, player.player_choice, @case]
-      @actual_board.play(@resume)
+end
 
-    if @actual_board.victory?
-      @actual_board.look_board
-      puts
-      puts "  #{player.name},gagne!    "
-      puts 'rejouer?'
-      puts '(Y/N)'
-      puts
-      answer = gets.chomp.to_s.capitalize
-      if answer == 'Y'
-        Game.new.go
-      else
-        puts 'bye'
+# class Boardase
+# Remplie le tableau a chaque appel (lorsque le joueur a choisi sa case)
+class BoardCase
+  attr_accessor :tab, :chosen_case, :symbol, :count
+
+  def initialize(tab, chosen_case, symbol, count)
+    @tab = tab
+    @chosen_case = chosen_case
+    @symbol = symbol
+    @count = count
+  end
+
+  def board_update
+    system 'clear'
+    if @tab[@chosen_case - 1] == ' '
+      @tab[@chosen_case - 1] = @symbol
+      x = 0
+      y = 0
+      while @tab[x]
+        if [1, 3, 6, 8, 11, 13].include?(y)
+          print '|'
+        elsif [2, 5].include?(x)
+          print " #{@tab[x]} "
+          print "\n-----------\n"
+          x += 1
+        else
+          print " #{@tab[x]} "
+          x += 1
+        end
+        y += 1
       end
-      break
+      print "\n"
+    else
+      puts 'Case occupée !'
+      sleep(1)
+      @count -= 1
     end
+    return @tab, @count
+  end
+end
 
-      turns += 1
+# Class Verification
+# Verifie si le jeu trouve un gagnant ou une egalite
+class Verification
+  attr_accessor :tab, :count
 
-    if turns == 9
-      @actual_board.look_board
-      puts
-      puts 'aucun gagnant'
-      puts 'rejouer?'
-      puts '(Y/N)'
-      puts
-      answer = gets.chomp.to_s.capitalize
-      if answer == 'Y'
-        Game.new.go
-      else
-        puts 'bye'
-      end
-      break
-    end
-      }
+  def initialize(tab, count)
+    @tab = tab
+    @count = count
+  end
+
+  def is_finished
+    joueur = 0
+    if tab[0] == 'x'.red && tab[1] == 'x'.red && tab[2] == 'x'.red || tab[3] == 'x'.red && tab[4] == 'x'.red && tab[5] == 'x'.red || tab[6] == 'x'.red && tab[7] == 'x'.red && tab[8] == 'x'.red || tab[0] == 'x'.red && tab[3] == 'x'.red && tab[6] == 'x'.red || tab[1] == 'x'.red && tab[4] == 'x'.red && tab[7] == 'x'.red || tab[2] == 'x'.red && tab[5] == 'x'.red && tab[8] == 'x'.red || tab[0] == 'x'.red && tab[4] == 'x'.red && tab[8] == 'x'.red || tab[2] == 'x'.red && tab[4] == 'x'.red && tab[6] == 'x'.red
+      return 1
+    elsif tab[0] == 'o'.blue && tab[1] == 'o'.blue && tab[2] == 'o'.blue || tab[3] == 'o'.blue && tab[4] == 'o'.blue && tab[5] == 'o'.blue || tab[6] == 'o'.blue && tab[7] == 'o'.blue && tab[8] == 'o'.blue || tab[0] == 'o'.blue && tab[3] == 'o'.blue && tab[6] == 'o'.blue || tab[1] == 'o'.blue && tab[4] == 'o'.blue && tab[7] == 'o'.blue || tab[2] == 'o'.blue && tab[5] == 'o'.blue && tab[8] == 'o'.blue || tab[0] == 'o'.blue && tab[4] == 'o'.blue && tab[8] == 'o'.blue || tab[2] == 'o'.blue && tab[4] == 'o'.blue && tab[6] == 'o'.blue
+      return 2
+    elsif count == 8
+      return 3
+    else
+      return joueur
     end
   end
 end
 
-Game.new.go
+system 'clear'
+
+puts 'Nom du 1er joueur :'
+player1 = Player.new(gets.chomp)
+player1 = player1.name
+
+puts 'Nom du 2eme joueur :'
+player2 = Player.new(gets.chomp)
+player2 = player2.name
+
+print "\n"
+
+game = Board.new
+game = game.base_board
+count = 0
+help = true
+verif = true
+continue = true
+game_num = 1
+
+while continue == true
+  puts "Partie n°#{game_num}"
+  while verif == true && count <= 9
+    if help == true
+      puts '/!\\ Attention /!\\'
+      puts 'Voici une aide pour le choix des cases :'
+      puts "1 | 2 | 3\n---------\n4 | 5 | 6\n---------\n7 | 8 | 9"
+      help = false
+    end
+    puts 'Choisir une case entre 1 et 9 :'
+    choice = gets.chomp.to_i
+    if choice <= 9 && choice.positive?
+      if (count % 2).zero? && count != 1
+        game, count = BoardCase.new(game, choice, 'x'.red, count)
+      else
+        game, count = BoardCase.new(game, choice, 'o'.blue, count)
+      end
+      game, count = game.board_update
+      win = Verification.new(game, count)
+      win = win.is_finished
+      if win.zero?
+        count += 1
+      elsif win == 1
+        puts "#{player1} à gagné !"
+        verif = false
+      elsif win == 2
+        puts "#{player2} à gagné !"
+        verif = false
+      else
+        puts 'Égalité !'
+        verif = false
+      end
+    else
+      puts 'Entrez un nombre entre 1 et 9'
+    end
+  end
+  puts 'Voulez vous recommencer ? [o/n]'
+  var = gets.chomp
+  if var == 'o'
+    continue = true
+    game = Board.new
+    game = game.base_board
+    count = 0
+    help = true
+    verif = true
+    continue = true
+  else
+    continue = false
+  end
+  game_num += 1
+end
+ 
